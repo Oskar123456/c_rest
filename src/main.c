48bbe409d33b117234649f54a0ed569307c6c2b3
oskar123456 <cph-oh82@cphbusiness.dk>
2024-11-12 14:45:52 +0100

    pretty sql

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 {
     c_log_init(stderr, LOG_LEVEL_SUCCESS);
 
-    //PGconn* db_connection = db_connect();
+    PGconn* db_connection = db_connect();
 
     //bool sql_script_res = db_exec_script(db_connection, "resources/sql/task_schema_create.sql");
     //if (!sql_script_res)
@@ -33,14 +33,14 @@ int main(int argc, char *argv[])
     //task_new(my_task, "wash dishes", "quick 2");
     //task_create(db_connection, my_task);
     //bool res = task_get_by_id(db_connection, my_task, 1);
-    //db_print_table(db_connection, "task");
+    db_print_table(db_connection, "task");
 
     /*
      * START SERVER
      */
-    int exit_code = serve(argc, argv);
+    //int exit_code = serve(argc, argv);
 
-    //PQfinish(db_connection);
+    PQfinish(db_connection);
     return exit_code;
 }
 
